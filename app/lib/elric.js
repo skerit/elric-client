725167ce7bbc65fe86abcdb273f2327160b5dff7
Jelle De Loecker <jelle@kipdola.be>
2015-09-07 23:02:04 +0200

    Client updates

--- app/lib/elric.js
+++ app/lib/elric.js
@@ -1,3 +1,5 @@
+var os = require('os');
+
 /**
  * The Elric class
  *
@@ -8,6 +10,94 @@
  * @version  1.0.0
  */
 var Elric = Function.inherits('Informer', function Elric() {
+
+	// Set the hostname
+	this.hostname = os.hostname();
+
+	log.info('Client will be identified as ' + JSON.stringify(this.hostname));
+
+	// Are we already connecting?
+	this._connecting = false;
+
+	// The found master packet
+	this.master = null;
+
+});
+
+/**
+ * Search and connect to the master
+ *
+ * @author   Jelle De Loecker   <jelle@develry.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ */
+Elric.setMethod(function connect(callback) {
+
+	var that = this;
+
+	if (callback) {
+		this.after('master', function gotMaster() {
+			callback(null, that.master);
+		});
+	}
+
+	if (this._connecting) {
+		return;
+	}
+
+	if (!callback) {
+		callback = Function.thrower;
+	}
+
+	this._connecting = true;
+
+	// Send out an multicast discovery
+	alchemy.discover('elric::master', function gotResponses(err, responses) {
+
+		var announce_data,
+		    master_uri,
+		    connection;
+
+		if (err) {
+			return callback(err);
+		}
+
+		if (!responses.length) {
+			// @todo: Try again?
+			return log.error('No master instance could be found ...');
+		}
+
+		announce_data = {
+			type: 'ElricClient',
+			hostname: that.hostname
+		};
+
+		// Set the response packet as master info
+		that.master = responses[0][1];
+
+		log.info('Found master at ' + that.master.remote.address);
+
+		master_uri = 'http://' + that.master.remote.address + ':' + that.master.http_port;
+
+		connection = alchemy.callServer(master_uri, announce_data, function connected(err) {
+
+			// Set the websocket connection
+			that.connection = connection;
+
+			if (!err) {
+				log.info('Connection made with master Elric instance');
+			} else {
+				log.error('Could not connect to master Elric instance');
+			}
+		});
+
+		connection.on('close', function closed() {
+			log.info('Lost connection to master');
+		});
+
+		// Emit the master event
+		that.emit('master', that.master);
+	});
 });
 
 // Create the elric singleton
--- client.js
+++ client.js
@@ -3,22 +3,54 @@
  */
 require('alchemymvc');
 
-setTimeout(function() {
-	console.log('Submitting data');
-alchemy.multicast('client', 'this is data from the client', true, function(response, packet, callback) {
-	console.log('Got client response', response);
+// Intercept uncaught exceptions so the server won't crash
+// @todo: this should be expanded and integrated into alchemy itself
+process.on('uncaughtException', function(error) {
+
+	// Indicate we caught an exception
+	log.error('Uncaught Exception!', {err: error});
+
+	console.log(error.stack)
 });
-}, 200)
+
+alchemy.start({client_mode: true}, function ready() {
+
+	// Create a connection to the master
+	elric.connect();
+
+});
+
+
+
+
+return;
+
+console.log('Submitting data');
+
+var text = 'this is data from the client';
+
+alchemy.multicast('client', text, true, function(responses) {
+	console.log('Got client response', responses);
+});
+
+// alchemy.callServer('192.168.1.2:3000', function() {
+// 	console.log('Connected!');
+// })
+
 
 return;
 
 var local    = require('./local'),
     address  = 'ws://' + local.server + ':' + local.serverport;
 
-alchemy.callServer(address, function(server) {
+alchemy.callServer(address, {type: 'elric_client'}, function(server) {
 
 	log.verbose('Established connection to Elric server at ' + (local.server + ':' + local.serverport).bold);
 
 	elric.server = server;
+
+	server.on('settings', function(settings) {
+		pr(settings);
+	});
 
 });
