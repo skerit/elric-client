c9b70e66ded3082cb923e7f06196750a2075c4fa
Jelle De Loecker <jelle@develry.be>
2017-08-07 16:47:03 +0200

    Add multicast ipv4 address

--- app/config/default.js
+++ app/config/default.js
@@ -1,4 +1,10 @@
 module.exports = {
+	// Multicast ipv4 address
+	multicast_ipv4: '230.185.192.47',
+
+	// Start listening to multicast on boot?
+	multicast_on_boot: true,
+
 	// Do an extensive and expensive search for modules
 	search_for_modules: true
 };
